090e0b638af6d406a096ba5ea8015ae953a6ffad
Jun Su <csujun@hotmail.com>
2013-01-16 20:33:23 +0800

    Enable a profiling data output through Port5

--- core/sys/energest.h
+++ core/sys/energest.h
@@ -90,6 +90,7 @@ extern energest_t energest_leveldevice_current_leveltime[ENERGEST_CONF_LEVELDEVI
                            /*++energest_total_count;*/ \
                            energest_current_time[type] = RTIMER_NOW(); \
 			   energest_current_mode[type] = 1; \
+                           P5OUT |= BV(type); \
                            } while(0)
 #ifdef __AVR__
 /* Handle 16 bit rtimer wraparound */
@@ -111,6 +112,7 @@ extern energest_t energest_leveldevice_current_leveltime[ENERGEST_CONF_LEVELDEVI
                            energest_total_time[type].current += (rtimer_clock_t)(RTIMER_NOW() - \
                            energest_current_time[type]); \
 			   energest_current_mode[type] = 0; \
+                           P5OUT &= ~BV(type); \
                            } while(0)
 
 #define ENERGEST_OFF_LEVEL(type,level) do { \
