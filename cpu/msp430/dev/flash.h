2ced011304d0924faf75e37062c14f48507de2b3
Jun Su <csujun@hotmail.com>
2013-03-02 23:20:34 +0800

    add a function to flash a whole segment.

--- cpu/msp430/dev/flash.h
+++ cpu/msp430/dev/flash.h
@@ -1,7 +1,7 @@
 /**
  * \file
  * Functions for reading and writing MSP430 flash ROM.
- * \author Adam Dunkels <adam@sics.se> 
+ * \author Adam Dunkels <adam@sics.se>
  */
 
 /* Copyright (c) 2004 Swedish Institute of Computer Science.
@@ -68,11 +68,13 @@ void flash_write(unsigned short *addr, unsigned short word);
  * Clear a 16-bit word in flash ROM.
  *
  * This function clears a 16-bit word in flash ROM. The function
- * flash_setup() must have been called first. 
+ * flash_setup() must have been called first.
  *
  * \param addr A pointer to the address in flash ROM which is to be
  * cleared.
  */
 void flash_clear(unsigned short *addr);
+
+void flash_writepage(uint16_t *addr, const uint16_t *data, uint8_t size);
 
 #endif /* __FLASH_H__ */
