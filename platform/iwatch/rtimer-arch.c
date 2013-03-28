e607a070772a980792a3031fbed1308b2e40cc6e
Jun Su <csujun@hotmail.com>
2013-03-28 21:49:50 +0800

    Move clock and rtimer to B0

--- platform/iwatch/rtimer-arch.c
+++ platform/iwatch/rtimer-arch.c
@@ -54,7 +54,7 @@
 #endif
 
 /*---------------------------------------------------------------------------*/
-ISR(TIMER1_A0, timera0)
+ISR(TIMER0_B0, timerb0)
 {
   ENERGEST_ON(ENERGEST_TYPE_IRQ);
 
@@ -77,7 +77,7 @@ rtimer_arch_init(void)
   dint();
 
   /* CCR0 interrupt enabled, interrupt occurs when timer equals CCR0. */
-  TA1CCTL0 = CCIE;
+  TB0CCTL0 = CCIE;
 
   /* Enable interrupts. */
   eint();
@@ -88,8 +88,8 @@ rtimer_arch_now(void)
 {
   rtimer_clock_t t1, t2;
   do {
-    t1 = TA1R;
-    t2 = TA1R;
+    t1 = TB0R;
+    t2 = TB0R;
   } while(t1 != t2);
   return t1;
 }
@@ -99,6 +99,6 @@ rtimer_arch_schedule(rtimer_clock_t t)
 {
   PRINTF("rtimer_arch_schedule time %u\n", t);
 
-  TA1CCR0 = t;
+  TB0CCR0 = t;
 }
 /*---------------------------------------------------------------------------*/
