5827ff9f8d00b1c006fa0e40f4aba9fab35514f9
Jun Su <csujun@hotmail.com>
2013-04-11 22:53:52 +0800

    Revert "transport reuse hci ougoing buffer as input buffer. need more testing but this save 255 bytes RAM."
    This reverts commit a571511587825b385ab0102f074b28c894ba1498.

--- platform/iwatch/btstack/src/hci_transport_h4_ehcill_dma.c
+++ platform/iwatch/btstack/src/hci_transport_h4_ehcill_dma.c
@@ -118,7 +118,7 @@ static uint8_t      ehcill_command_to_send;
 static H4_STATE  h4_state;
 static int       read_pos;
 static int       bytes_to_read;
-static uint8_t   *hci_packet; // bigger than largest packet
+static uint8_t   hci_packet[HCI_PACKET_BUFFER_SIZE]; // bigger than largest packet
 static  void (*packet_handler)(uint8_t packet_type, uint8_t *packet, uint16_t size) = dummy_handler;
 
 // H4: tx state
@@ -170,9 +170,8 @@ static void h4_register_packet_handler(void (*handler)(uint8_t packet_type, uint
 
 static int h4_open(void *transport_config){
 
-    hci_packet = hci_get_outgoing_acl_packet_buffer();
 	// open uart
-    hal_uart_dma_init();
+	hal_uart_dma_init();
     hal_uart_dma_set_block_received(h4_block_received);
     hal_uart_dma_set_block_sent(h4_block_sent);
     hal_uart_dma_set_csr_irq_handler(ehcill_cts_irq_handler);
