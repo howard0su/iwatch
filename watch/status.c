9651586ee4abb929028cca844e1dd3b956aee034
Jun Su <csujun@hotmail.com>
2013-05-14 22:09:51 +0800

    add statusbar skelton

---
+++ watch/status.c
@@ -0,0 +1,15 @@
+#include "contiki.h"
+#include "sys/ctimer.h"
+
+#include "grlib/grlib.h"
+
+void status_draw(tContext *pContext)
+{
+  // if we need draw text
+
+  // if we need draw icon
+
+  // update activity completion
+
+}
+
