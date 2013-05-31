dc3f6473124656c513dff04dc799162075d5fc99
Jun Su <csujun@hotmail.com>
2013-05-31 23:52:25 +0800

    Add font generation

---
+++ tools/Fonts/run.bat
@@ -0,0 +1,14 @@
+
+ftrasterize.exe -f Red -s 13  "C&C Red Alert [LAN].ttf"
+ftrasterize.exe -f Baby -s 16  babyblue.ttf
+ftrasterize.exe -f Baby -s 12  babyblue.ttf
+
+
+
+ftrasterize.exe -f Nova -s 10  ProximaNova-Regular.otf
+ftrasterize.exe -f Nova -s 12  ProximaNova-Regular.otf
+ftrasterize.exe -f Nova -s 28  ProximaNova-Regular.otf
+ftrasterize.exe -f Nova -s 9 -b  ProximaNova-Bold.otf
+ftrasterize.exe -f Nova -s 12 -b  ProximaNova-Bold.otf
+ftrasterize.exe -f Nova -s 28 -b  ProximaNova-Bold.otf
+ftrasterize.exe -f Nova -s 38 -b  ProximaNova-Bold.otf
