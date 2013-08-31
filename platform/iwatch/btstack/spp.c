e07f2be9421c6f1847d98e85e4f3ef728516adf5
cowfinger <cowfinger@outlook.com>
2013-08-31 09:46:58 +0800

    move transport layer outside spp.c and build stlvTest

--- platform/iwatch/btstack/spp.c
+++ platform/iwatch/btstack/spp.c
@@ -9,6 +9,7 @@
 #include "config.h"
 #include "stlv.h"
 #include "stlv_server.h"
+#include "stlv_transport.h"
 #include "debug.h"
 
 static service_record_item_t spp_service_record;
@@ -23,148 +24,10 @@ static const uint8_t   spp_service_buffer[100] = {
 };
 
 #define SPP_CHANNEL 1
-static uint16_t spp_channel_id = 0;
+uint16_t spp_channel_id = 0;
 
 static char spp_buffer[1024];
 static uint16_t spp_read_ptr = 0, spp_write_ptr = 0;
-
-
-#define SPP_SENDER_NULL     0
-#define SPP_SENDER_READY    1
-#define SPP_SENDER_SENDING  2
-#define SPP_SENDER_SENT     3
-
-#define SPP_PACKET_SIZE  40
-
-#define SPP_FLAG_BEGIN       0x01
-#define SPP_FLAG_END         0x02
-#define SPP_FLAG_BEGIN_END   (SPP_FLAG_BEGIN | SPP_FLAG_END)
-
-typedef struct _spp_sender
-{
-    uint8_t* buffer;
-    short    buffer_size;
-    short    sent_size;
-    short    status;
-    short    unit_size;
-    void    (*callback)(int);
-    int     para;
-}spp_sender;
-
-#define TASK_QUEUE_SIZE 10
-static spp_sender task_queue[TASK_QUEUE_SIZE] = {0};
-static short task_queue_pos = 0;
-
-void tryToSend(void);
-int spp_register_task(uint8_t* buf, int size, void (*callback)(int), int para)
-{
-    short cursor = task_queue_pos + 1;
-    for (int i = 0; i < TASK_QUEUE_SIZE; ++i)
-    {
-        if (cursor >= TASK_QUEUE_SIZE)
-            cursor = 0;
-
-        spp_sender* task = &task_queue[cursor];
-        if (task->status == SPP_SENDER_NULL)
-        {
-            task->buffer        = buf;
-            task->buffer_size   = size;
-            task->sent_size     = 0;
-            task->unit_size     = 0;
-            task->callback      = callback;
-            task->para          = para;
-            task->status        = SPP_SENDER_READY;
-            tryToSend();
-            return 0;
-        }
-        cursor++;
-    }
-    return -1;
-}
-
-static uint8_t build_transport_packet(spp_sender* task)
-{
-    short left_size = task->buffer_size - task->sent_size;
-    short send_size = left_size > SPP_PACKET_SIZE ? SPP_PACKET_SIZE : left_size;
-
-    uint8_t* flag_ptr = task->buffer + task->sent_size - 1;
-    *flag_ptr = 0;
-    if (send_size == left_size)
-        *flag_ptr |= SPP_FLAG_END;
-    if (task->sent_size == 0)
-        *flag_ptr |= SPP_FLAG_BEGIN;
-
-    return send_size;
-}
-
-static void tryToSend(void){
-    if (!spp_channel_id) return;
-
-    for (int i = 0; i < TASK_QUEUE_SIZE; ++i)
-    {
-        if (task_queue_pos >= TASK_QUEUE_SIZE)
-            task_queue_pos = 0;
-
-        spp_sender* task = &task_queue[task_queue_pos];
-
-        if (task->status == SPP_SENDER_READY)
-        {
-            task->unit_size = build_transport_packet(task);
-            task->status = SPP_SENDER_SENDING;
-        }
-
-        if (task->status == SPP_SENDER_SENDING)
-        {
-            int err = rfcomm_send_internal(spp_channel_id,
-                task->buffer + task->sent_size - 1, task->unit_size + 1);
-            if (err != 0)
-                return;
-
-            task->sent_size += task->unit_size;
-            if (task->sent_size >= task->buffer_size)
-            {
-                if (task->callback != NULL)
-                    task->callback(task->para);
-                task->status = SPP_SENDER_NULL;
-                task_queue_pos++;
-            }
-            else
-            {
-                task->status = SPP_SENDER_READY;
-                task->unit_size = 0;
-            }
-            return;
-        }
-
-        task_queue_pos++;
-    }
-    return;
-
-}
-
-
-static unsigned char _recv_packet[STLV_PACKET_MAX_SIZE];
-static short _recv_packet_size = 0;
-
-static short handle_stvl_transport(unsigned char* packet, uint16_t size)
-{
-    if ((packet[0] & SPP_FLAG_BEGIN) != 0)
-        _recv_packet_size = 0;
-
-    if (_recv_packet_size + size - 1 > STLV_PACKET_MAX_SIZE)
-    {
-        _recv_packet_size = 0;
-        return -1;
-    }
-
-    memcpy(&_recv_packet[_recv_packet_size], packet + 1, size - 1);
-    _recv_packet_size += (size - 1);
-
-    if ((packet[0] & SPP_FLAG_END) != 0)
-        return _recv_packet_size;
-    else
-        return 0;
-}
 
 static void spp_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
 {
@@ -175,8 +38,8 @@ static void spp_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, 
     hexdump(packet, size);
     if (handle_stvl_transport(packet, size) > 0)
     {
-        handle_stlv_packet(_recv_packet);
-        _recv_packet_size = 0;
+        handle_stlv_packet(get_stlv_transport_buffer());
+        reset_stlv_transport_buffer();
     }
     return;
   }
---
+++ platform/iwatch/btstack/stlv_transport.c
@@ -0,0 +1,150 @@
+
+#include "stlv_transport.h"
+
+#include <stdio.h>
+#include "stlv.h"
+
+#ifdef UNITTEST
+#   define send_internal test_send_internal
+#else
+#   define send_internal rfcomm_send_internal
+#include "rfcomm.h"
+#endif
+
+typedef struct _spp_sender
+{
+    uint8_t* buffer;
+    short    buffer_size;
+    short    sent_size;
+    short    status;
+    short    unit_size;
+    void    (*callback)(int);
+    int     para;
+}spp_sender;
+
+#define TASK_QUEUE_SIZE 10
+static spp_sender task_queue[TASK_QUEUE_SIZE] = {0};
+static short task_queue_pos = 0;
+
+int spp_register_task(uint8_t* buf, int size, void (*callback)(int), int para)
+{
+    short cursor = task_queue_pos + 1;
+    for (int i = 0; i < TASK_QUEUE_SIZE; ++i)
+    {
+        if (cursor >= TASK_QUEUE_SIZE)
+            cursor = 0;
+
+        spp_sender* task = &task_queue[cursor];
+        if (task->status == SPP_SENDER_NULL)
+        {
+            task->buffer      = buf;
+            task->buffer_size = size;
+            task->sent_size   = 0;
+            task->unit_size   = 0;
+            task->callback    = callback;
+            task->para        = para;
+            task->status      = SPP_SENDER_READY;
+            tryToSend();
+            return 0;
+        }
+        cursor++;
+    }
+    return -1;
+}
+
+static uint8_t build_transport_packet(spp_sender* task)
+{
+    short left_size = task->buffer_size - task->sent_size;
+    short send_size = left_size > SPP_PACKET_SIZE ? SPP_PACKET_SIZE : left_size;
+
+    uint8_t* flag_ptr = task->buffer + task->sent_size - 1;
+    *flag_ptr = 0;
+    if (send_size == left_size)
+        *flag_ptr |= SPP_FLAG_END;
+    if (task->sent_size == 0)
+        *flag_ptr |= SPP_FLAG_BEGIN;
+
+    return send_size;
+}
+
+extern uint16_t spp_channel_id;
+void tryToSend(void)
+{
+    if (!spp_channel_id) return;
+
+    for (int i = 0; i < TASK_QUEUE_SIZE; ++i)
+    {
+        if (task_queue_pos >= TASK_QUEUE_SIZE)
+            task_queue_pos = 0;
+
+        spp_sender* task = &task_queue[task_queue_pos];
+
+        if (task->status == SPP_SENDER_READY)
+        {
+            task->unit_size = build_transport_packet(task);
+            task->status = SPP_SENDER_SENDING;
+        }
+
+        if (task->status == SPP_SENDER_SENDING)
+        {
+            int err = send_internal(spp_channel_id,
+                task->buffer + task->sent_size - 1, task->unit_size + 1);
+            if (err != 0)
+                return;
+
+            task->sent_size += task->unit_size;
+            if (task->sent_size >= task->buffer_size)
+            {
+                if (task->callback != NULL)
+                    task->callback(task->para);
+                task->status = SPP_SENDER_NULL;
+                task_queue_pos++;
+            }
+            else
+            {
+                task->status = SPP_SENDER_READY;
+                task->unit_size = 0;
+            }
+            return;
+        }
+
+        task_queue_pos++;
+    }
+    return;
+
+}
+
+static uint8_t _recv_packet[STLV_PACKET_MAX_SIZE];
+static short _recv_packet_size = 0;
+
+void reset_stlv_transport_buffer()
+{
+    _recv_packet_size = 0;
+}
+
+uint8_t* get_stlv_transport_buffer()
+{
+    return _recv_packet;
+}
+
+short handle_stvl_transport(unsigned char* packet, uint16_t size)
+{
+    if ((packet[0] & SPP_FLAG_BEGIN) != 0)
+        _recv_packet_size = 0;
+
+    if (_recv_packet_size + size - 1 > STLV_PACKET_MAX_SIZE)
+    {
+        _recv_packet_size = 0;
+        return -1;
+    }
+
+    memcpy(&_recv_packet[_recv_packet_size], packet + 1, size - 1);
+    _recv_packet_size += (size - 1);
+
+    if ((packet[0] & SPP_FLAG_END) != 0)
+        return _recv_packet_size;
+    else
+        return 0;
+}
+
+
