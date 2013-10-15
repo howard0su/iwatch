def7b2db9d47d2f164a618a9b9b24eedb806fb25
Howard Su <howard0su@gmail.com>
2013-10-15 21:30:42 +0800

    new agorithm

--- pedometer/pedometer.c
+++ pedometer/pedometer.c
@@ -5,16 +5,13 @@
 #define SAMPLE_HZ 50
 
 unsigned short step_cnt;
-static uint8_t sample_count;
-static uint16_t sample_new, sample_old;
-static uint16_t min_sample, max_sample;
-static uint16_t dc;
 static uint16_t interval;
-static int8_t mode; // -4 is default value, when > 0, means in confirmd mode
-static uint16_t precision;
+
+static int16_t last[3];
 
 void ped_step_detect_init(void)
 {
+#if 0
   sample_count = 0;
   sample_new = sample_old = 0;
   min_sample = 65535;
@@ -22,35 +19,70 @@ void ped_step_detect_init(void)
   dc = 65535 / 2;
   interval = 0;
   mode = -4;
-  precision = (max_sample - min_sample)/16;
+  precision = (max_sample - min_sample)/4;
+#endif
+  last[0] = last[1] = last[2] = 0;
 }
 
 unsigned short ped_step_detect(void)
 {
+  return step_cnt;
 }
 
-static uint16_t totalaccel(int16_t *data)
+static int16_t filter(int16_t value, int16_t *slot)
 {
-  uint16_t total = 0;
+  int16_t ret = value - *slot; 
+  *slot = ret / 8 + *slot;
+  
+  return ret;
+}
+
+static int16_t filter2(int16_t value)
+{
+  static int32_t lastsample = 0;
+  
+  uint32_t ret;
+  if (value > lastsample)
+    ret = (value - lastsample) / 2 + lastsample;
+  else
+    ret = (value - lastsample) / 16 + lastsample;
+  
+  lastsample = ret;
+  
+  return ret / 2; // gt = 1/2 
+}
+
+static int16_t totalaccel(short *data)
+{
+  int16_t total = 0;
+  
   // compute each axis abs
   for(int i = 0; i < 3; i++)
   {
-    int16_t v = data[i];
+    int16_t v = filter(data[i], &last[i]);
     if (v < 0)
       v = -v;
-
-     total += v/2;
+    
+    total += v/2;
   }
   
   return total;
 }
 
 
-char ped_update_sample(signed short *data)
+char ped_update_sample(short *data)
 {
+ //printf("%d\t%d\t%d\t", data[0], data[1], data[2]);
+#if 0
   // cacluate the total accel
   uint16_t sample = totalaccel(data);
-  
+  static uint8_t sample_count;
+  static uint16_t sample_new, sample_old;
+  static uint16_t min_sample, max_sample;
+  static uint16_t dc;
+  static uint16_t precision;
+  static int8_t mode;
+
   if (sample > max_sample)
     max_sample = sample;
   if (sample < min_sample)
@@ -63,18 +95,17 @@ char ped_update_sample(signed short *data)
   {
     sample_count = 0;
     dc = (min_sample + max_sample)/2;
-    precision = (max_sample - min_sample)/16;
+    precision = (max_sample - min_sample)/4;
     printf("min=%d max=%d dc = %d precisioin=%d\n", min_sample, max_sample, dc, precision);
     min_sample = 65535;
-    max_sample = 0;    
-    
+    max_sample = 0;
   }
   
-  printf("%d: [%d] (new:%d old:%d) ", sample_count, sample, sample_new, sample_old);
+  printf("[%d] (%d -> %d) ", sample, sample_new, sample_old);
   sample_old = sample_new;
   if (sample - sample_new < precision || sample - sample_new > -precision)
   {
-    printf("noise\n");
+    printf("\n");
     return 1;
   }
   
@@ -106,6 +137,41 @@ char ped_update_sample(signed short *data)
     }
   }
   
-  printf(" mode=%d\n", mode);
+  printf(" %d\n", mode);
+  
+#else
+  int16_t total = totalaccel(data); 
+  int16_t delta = filter2(total);
+  
+  static int holdoff = 0;
+  interval++;
+  
+  if (holdoff > 0)
+  {
+    holdoff--;
+  }
+  else if (holdoff < 0)
+  {
+    if (total > delta)
+    {
+    }
+    else
+    {
+      holdoff = SAMPLE_HZ/5;
+    }
+  }
+  else if (total > delta)
+  {
+    //if (interval > SAMPLE_HZ/5 && interval < SAMPLE_HZ * 2)
+    {
+      step_cnt++;
+      interval = 0;
+    }
+    holdoff = -1;
+  }
+
+ //printf("| %d\t%d\t%d\t%d\n", total, delta, holdoff, step_cnt);
+  
+#endif
   return 1;
 }
