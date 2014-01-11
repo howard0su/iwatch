991c41a3b3517e4fc89289f0cd5df50ef5af72ed
Howard Su <howard0su@gmail.com>
2014-01-11 22:28:31 +0800

    share the data inside ant+ stack.

---
+++ ant/data.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "config.h"
+#include "antinterface.h"
+#include "antmessage.h"
+#include "antdefines.h"
+
+#include "data.h"
+
+union ant_data data;
+
+
+UCHAR ucAntChannel;
+USHORT usDeviceNumber;
+UCHAR ucTransType;
