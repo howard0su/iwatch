161610d8479aa87a2b6b5c3a249263b20f816027
Howard Su <howard0su@gmail.com>
2014-01-19 15:54:05 +0800

    Increase SPI bus speed to get screen refersh and spi read faster.

--- platform/iwatch/Template_Driver.c
+++ platform/iwatch/Template_Driver.c
@@ -151,7 +151,7 @@ static void SPIInit()
 
   UCB0CTL0 = UCMODE0 + UCMST + UCSYNC + UCCKPH; // master, 3-pin SPI mode, LSB
   UCB0CTL1 |= UCSSEL__SMCLK; // SMCLK for now
-  UCB0BR0 = 8; // 8MHZ / 8 = 1Mhz
+  UCB0BR0 = 2; // 8MHZ / 8 = 1Mhz
   UCB0BR1 = 0;
 
   //Configure ports.
